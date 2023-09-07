5b373e54aa04241c43113c3c939b2d752b241f54
andrew@tainan <dimsplendid@gmail.com>
2023-09-07 22:21:24 +0800

    Connect Pstgres Success
    The simple test in main is ok.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 #[macro_use]
 extern crate rocket;
 
-// TODO: import log, pretty_env_logger, dotenv, and PgPoolOptions
+extern crate pretty_env_logger;
+#[macro_use] extern crate log;
+
+use dotenvy::dotenv;
+use std::env;
+
+use sqlx::postgres::PgPoolOptions;
 
 mod cors;
 mod handlers;
@@ -12,23 +18,32 @@ use handlers::*;
 
 #[launch]
 async fn rocket() -> _ {
-    // TODO: Initialize pretty_env_logger
-    // TODO: Initialize dotenv
+    pretty_env_logger::init();
+
+    dotenv().expect(".env file should be in the root of the project");
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
     // Create a new PgPoolOptions instance with a maximum of 5 connections.
     // Use dotenv to get the database url. 
     // Use the `unwrap` or `expect` method instead of handling errors. If an
     // error occurs at this stage the server should be terminated. 
     // See examples on GitHub page: https://github.com/launchbadge/sqlx
-    // let pool = todo!();
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&db_url)
+        .await
+        .expect("Failed to create pool");
 
     // Using slqx, execute a SQL query that selects all questions from the questions table.
     // Use the `unwrap` or `expect` method to handle errors. This is just some test code to
     // make sure we can connect to the database.  
-    // let recs = todo!();
+    let recs = sqlx::query!("SELECT * FROM questions")
+        .fetch_all(&pool)
+        .await
+        .expect("Failed to fetch questions");
 
     info!("********* Question Records *********");
-    // TODO: Log recs with debug formatting using the info! macro
+    info!("{:?}", recs);
 
     rocket::build()
         .mount(
