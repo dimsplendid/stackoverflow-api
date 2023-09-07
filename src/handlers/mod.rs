c10005598cb51c18dbf97d1976f27b5b8a1d51dd
andrew.tseng <andrew.tseng@innolux.com>
2023-09-07 16:28:24 +0800

    Impl CRUD for answer and question

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -8,34 +8,67 @@ use crate::models::*;
 pub async fn create_question(
     question: Json<Question>,
 ) -> Json<QuestionDetail> {
-    todo!()
+    QuestionDetail {
+        question_uuid: "[UUID of a created question]".to_string(),
+        title: question.title,
+        description: question.description,
+        created_at: "2021-08-25T00:00:00Z".to_string(),
+    }
 }
 
 #[get("/questions")]
 pub async fn read_questions() -> Json<Vec<QuestionDetail>> {
-    todo!()
+    vec![QuestionDetail {
+        question_uuid: "[UUID of a created question]".to_string(),
+        title: "How do I create a REST API?".to_string(),
+        description: "I want to create a REST API using Rust and Rocket.".to_string(),
+        created_at: "2021-08-25T00:00:00Z".to_string(),
+    }]
 }
 
 #[delete("/question", data = "<question_uuid>")]
 pub async fn delete_question(
     question_uuid: Json<QuestionId>
 ) {
-    todo!()
+    QuestionId {
+        question_uuid: question_uuid.question_uuid,
+    }
 }
 
 // ---- CRUD for Answers ----
 
-// TODO: Create a POST route to /answer which accepts an `Answer` and returns `AnswerDetail` as JSON.
-//       The handler function should be called `create_answer`.
-//       
-//       hint: this function should look very similar to the create_question function above
+#[post("/answer", data = "<answer>")]
+pub async fn create_answer (
+    answer: Json<Answer>,
+) -> Json<AnswerDetail> {
+    AnswerDetail {
+        answer_uuid: "[UUID of a created answer]".to_string(),
+        question_uuid: answer.question_uuid,
+        content: answer.content,
+        created_at: "2021-08-25T00:00:00Z".to_string(),
+    }
+}
 
-// TODO: Create a GET route to /answers which accepts an `QuestionId` and returns a vector of `AnswerDetail` as JSON.
-//       The handler function should be called `read_answers`.
-//       
-//       hint: this function should look very similar to the read_questions function above
+#[get("/answers")]
+pub async fn read_answers() -> Json<Vec<AnswerDetail>> {
+    vec![AnswerDetail {
+        answer_uuid: "[UUID of a created answer]".to_string(),
+        question_uuid: "[UUID of a created question]".to_string(),
+        content: "You should use Rocket!".to_string(),
+        created_at: "2021-08-25T00:00:00Z".to_string(),
+    }]
+}
 
 // TODO: Create a DELETE route to /answer which accepts an `AnswerId` and does not return anything.
 //       The handler function should be called `delete_answer`.
 //       
 //       hint: this function should look very similar to the delete_question function above
+
+#[delete("/answer", data = "<answer_uuid>")]
+pub async fn delete_answer(
+    answer_uuid: Json<AnswerId>
+) {
+    AnswerId {
+        answer_uuid: answer_uuid.answer_uuid,
+    }
+}
--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct Question {
     pub title: String,
     pub description: String,
